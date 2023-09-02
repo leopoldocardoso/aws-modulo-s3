213c153cee930400b51d81ab8e2a01685b0b69d6
Leopoldo Cardoso <peixoto.leopoldo@outlook.com>
2023-09-02 11:37:18 -0300

    Criação de um simples módulo s3

--- object/s3.tf
+++ object/s3.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "s3" {
- bucket = var.bucket-name
- tags = var.tag
+  bucket = var.bucket-name
+  tags   = var.tag
 }
 
 
 resource "aws_s3_bucket_public_access_block" "block-access-s3" {
-  bucket = aws_s3_bucket.s3.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.s3.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
--- object/variables.tf
+++ object/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket-name" {
   description = "Nome do Bucket"
-  type = string
-  default = "iac-s3-leopoldo"
+  type        = string
+  default     = "iac-s3-leopoldo"
 }
 
 variable "tag" {
@@ -9,8 +9,8 @@ variable "tag" {
   type        = map(any)
   default = {
     Owner     = "Leopoldo P. Cardoso"
-      ManagedBy = "Terraform"
-      Objective = "Estudos Terraform"
-      Name = "iac-s3-leopoldo"
+    ManagedBy = "Terraform"
+    Objective = "Estudos Terraform"
+    Name      = "iac-s3-leopoldo"
   }
 }
